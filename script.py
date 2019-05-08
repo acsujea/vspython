e7795a0580a7e056bf34d5be329092134d09c966
Andy <acsujea@gmail.com>
2019-05-07 22:09:46 -0400

    added

--- script.py
+++ script.py
@@ -1,19 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-print("Python version: {}".format(sys.version))
-print("Python path: {}".format(sys.executable))
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-print(greet("World"))
-print(greet("Corey"))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
